64faadd71cebdce913c62506b024c2612944c624
Eric Eisenhart <freiheit@gmail.com>
2025-01-19 21:27:04 -0800

    Style/formatting fixups with `black`

--- watch.py
+++ watch.py
@@ -1,4 +1,4 @@
-#!/opt/kwikset/venv/bin/python 
+#!/opt/kwikset/venv/bin/python
 
 import aiodns
 import aiohttp
@@ -10,29 +10,32 @@ import time
 from aiokwikset import API
 from pprint import pprint
 
+
 async def main() -> None:
     """Run!"""
 
+    confparse = configargparse.ArgParser(
+        default_config_files=["./kwikset.conf", "/etc/kwikset.conf", "~/.kwikset.conf"]
+    )
+    confparse.add("-u", "--username", required=True)
+    confparse.add("-p", "--password", required=True)
+    confparse.add("--heartbeaturl", required=False, default=False)
 
-    confparse = configargparse.ArgParser(default_config_files=['./kwikset.conf', '/etc/kwikset.conf', '~/.kwikset.conf'])
-    confparse.add('-u', '--username', required=True)
-    confparse.add('-p', '--password', required=True)
-    confparse.add('--heartbeaturl', required=False, default=False)
-    
     conf = confparse.parse_args()
 
-    #initialize the API
+    # initialize the API
     logging.basicConfig(
-        filename='/var/log/kwikset.log',
-        format='%(asctime)s %(levelname)-8s %(message)s',
+        filename="/var/log/kwikset.log",
+        format="%(asctime)s %(levelname)-8s %(message)s",
         level=logging.INFO,
-        datefmt='%Y-%m-%d %H:%M:%S')
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
     api = API(conf.username)
 
-    pre_auth = await api.authenticate(conf.password ,'phone')
+    pre_auth = await api.authenticate(conf.password, "phone")
 
-    #MFA verification
+    # MFA verification
     await api.verify_user(pre_auth, input("Code:"))
 
     # Get user account information:
@@ -42,15 +45,20 @@ async def main() -> None:
     homes = await api.user.get_homes()
 
     # Get the devices for the first home
-    devices = await api.device.get_devices(homes[0]['homeid'])
+    devices = await api.device.get_devices(homes[0]["homeid"])
 
     async with aiohttp.ClientSession() as http:
-        while(True):
-            device_info = await api.device.get_device_info(devices[0]['deviceid'])
+        while True:
+            device_info = await api.device.get_device_info(devices[0]["deviceid"])
             # pprint(device_info)
-            logging.info(f"{device_info['batterypercentage']}% {device_info['batterystatus']}")
+            logging.info(
+                f"{device_info['batterypercentage']}% {device_info['batterystatus']}"
+            )
             if conf.heartbeaturl:
-                await http.get('https://heartbeat.uptimerobot.com/m798367254-c8f5713329ac5a83ce29a23a97af65c20a902e6e')   
+                await http.get(
+                    "https://heartbeat.uptimerobot.com/m798367254-c8f5713329ac5a83ce29a23a97af65c20a902e6e"
+                )
             await asyncio.sleep(300)
+
 
 asyncio.run(main())
