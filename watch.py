cb53df2c7e17fb4f1366c92eb18bc62d25ba8865
Eric Eisenhart <freiheit@gmail.com>
2025-02-08 18:02:32 -0800

    Updates for sending data to discord: - Has discord webhook support now - Made detailstime and sleeptime configurable - Changed default detailstime from 1h to 2h

--- watch.py
+++ watch.py
@@ -6,9 +6,10 @@ import asyncio
 import configargparse
 import logging
 import time
+import json
 
 from aiokwikset import API
-from pprint import pprint
+from pprint import pprint, pformat
 
 
 async def main() -> None:
@@ -20,6 +21,9 @@ async def main() -> None:
     confparse.add("-u", "--username", required=True)
     confparse.add("-p", "--password", required=True)
     confparse.add("--heartbeaturl", required=False, default=False)
+    confparse.add("-d", "--detailstimesecs", required=False, default=7200)
+    confparse.add("-s", "--sleeptime", required=False, default=300)
+    confparse.add("--discordhookurl", required=False, default=False)
 
     conf = confparse.parse_args()
 
@@ -48,15 +52,36 @@ async def main() -> None:
     devices = await api.device.get_devices(homes[0]["homeid"])
 
     async with aiohttp.ClientSession() as http:
+        details_time = 0
+        last_percentage = 100
         while True:
             device_info = await api.device.get_device_info(devices[0]["deviceid"])
             # pprint(device_info)
             logging.info(
                 f"{device_info['batterypercentage']}% {device_info['batterystatus']}"
             )
+            if (
+                last_percentage != device_info["batterypercentage"]
+                or (details_time + conf.detailstimesecs) < time.time()
+            ):
+                logging.info(json.dumps(device_info))
+                last_percentage = device_info["batterypercentage"]
+                if conf.discordhookurl:
+                    discordpayload = {
+                        "username": "Kwikset Lock",
+                        "content": f"# Battery Percent: {device_info['batterypercentage']}\n"
+                        f"## Battery Status: {device_info['batterystatus']}\n"
+                        f"## Last Lock Status Time: {time.ctime(device_info['lastLockStatusTime'])}\n"
+                        f"## Last Update Status: {time.ctime(device_info['lastupdatestatus'])}\n"
+                        "```json\n"
+                        f"{json.dumps(device_info,indent=2)}\n"
+                        "```\n",
+                    }
+                    await http.post(conf.discordhookurl, json=discordpayload)
+
             if conf.heartbeaturl:
                 await http.get(conf.heartbeaturl)
-            await asyncio.sleep(300)
+            await asyncio.sleep(conf.sleeptime)
 
 
 asyncio.run(main())
